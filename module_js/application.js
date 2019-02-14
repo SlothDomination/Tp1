71bd3605853bd58e81d5c39c2e6c2588966317ed
SlothDomination <e1630725@cmaisonneuve.qc.ca>
2019-02-14 18:42:44 -0500

    CSS recommencer----

--- module_js/application.js
+++ module_js/application.js
@@ -1,28 +1,28 @@
 
-/*import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
-//import {Introduction} from './Introduction.js' // 
-//import {AnimLettre} from './AnimLettre.js' //
+import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
+import {Introduction} from './Introduction.js' // 
+import {AnimLettre} from './AnimLettre.js' //
 
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
-//let elmHeader = document.querySelector('.header');
-//let intro = new Introduction(contenuIntro, elmHeader, animationLettre);/*intro(truc, truc, suivante anim a jouer) */
+let elmHeader = document.querySelector('.header');
+let intro = new Introduction(contenuIntro, elmHeader, animationLettre);/*intro(truc, truc, suivante anim a jouer) */
 
 // debutQuestionnaire()
-/*
+
 function animationLettre()
 {
     /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
-/*	console.log('debut animation lettre');
+	console.log('debut animation lettre');
 	const lesLettres = 'Veille-technologique';
  	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim);/*(truc, conteneur parent, fonction)*/
-/*}
+}
 
 
 function finAnim()
 {
 	console.log('animation terminée');
 }
-*/
+
 
 
 
