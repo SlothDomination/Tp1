f26e33a8879135243eed85fdb193b77aace42632
SlothDomination <e1630725@cmaisonneuve.qc.ca>
2019-02-21 15:33:37 -0500

    ajouter information au html

--- module_js/application.js
+++ module_js/application.js
@@ -1,11 +1,28 @@
-
 import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
 import {Introduction} from './Introduction.js' // 
 import {AnimLettre} from './AnimLettre.js' //
 
 /* l'élement de la page qui contiendra les éléments créés dynamiquement */
-let elmHeader = document.querySelector('.header');
-let intro = new Introduction(contenuIntro, elmHeader, animationLettre);/*intro(truc, truc, suivante anim a jouer) */
+let elmSection = document.querySelector('section');
+//let elmButton = document.querySelector("input[type='button']");
+let i = 0;
+
+elmSection.onmousedown = function (){	
+	if(i == 0){
+		console.log(i);
+		i++;
+		let intro = new Introduction(contenuIntro, elmSection, animationLettre);//intro(truc, truc, suivante anim a jouer)
+	}
+
+	let elmDiv = document.querySelector("div.introduction");
+
+	if(elmDiv.style.display == "none" || elmDiv.style.display == ""){
+		elmDiv.style.display = "block";
+	}else{
+		elmDiv.style.display = "none";
+	}
+
+}
 
 // debutQuestionnaire()
 
@@ -14,7 +31,7 @@ function animationLettre()
     /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
 	console.log('debut animation lettre');
 	const lesLettres = 'Veille-technologique';
- 	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim);/*(truc, conteneur parent, fonction)*/
+ 	let  monAnimLettre = new AnimLettre(lesLettres, elmSection, finAnim);/*(truc, conteneur parent, fonction)*/
 }
 
 
