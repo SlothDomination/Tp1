5293ed9f3e4b2ec6d270622afdb33be60272fb8c
SlothDomination <soranamsarac@gmail.com>
2019-02-24 21:14:48 -0500

    Test1 JS pour que chaque section soit cliquable avec nom auteur correct -- echec

--- module_js/contenuIntro.js
+++ module_js/contenuIntro.js
@@ -1,11 +1,22 @@
 /* Contenu intro est un objet litéral qui contiendra 
     les éléments d'introduction
 */
-let idImage = document.querySelector('section').id;
+/*let i = 0;
+let Auteurs = ["CRISALYS", "Inconnu", "Ilya Kuvshinov", "meyoco", "tofuvi", "Inconnu"];
+let idImage;
+for(i= 0; i<Auteurs.length; i++){
+    idImage= document.querySelectorAll('section')[i].id;
+    console.log("contenuIntro: " + idImage);
+}
+
+export const contenuIntro = {    
+	description : "Auteur: " + Auteurs[idImage]
+}*/
+
+let idImage= document.querySelector('section').id;
 let Auteurs = ["CRISALYS", "Inconnu", "Ilya Kuvshinov", "meyoco", "tofuvi", "Inconnu"];
 
 
-export const contenuIntro = {
+export const contenuIntro = {    
 	description : "Auteur: " + Auteurs[idImage]
-
 }
